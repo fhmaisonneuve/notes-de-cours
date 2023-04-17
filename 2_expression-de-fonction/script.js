3bde315a6b646506ecea18e7764e161730eda510
Maxime Pigeon <me@maximepigeon.com>
2023-04-17 17:39:52 -0400

    Add expression de fonction notes

---
+++ 2_expression-de-fonction/script.js
@@ -0,0 +1,46 @@
+////////////////////////////////////////////////////////////////////////////////
+// EXPRESSION DE FONCTION IMMÉDIATEMENT INVOQUÉES //////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+// IIFE (Immediately Invoked Function Expression) (Expression de fonction
+// invoquée immédiatement) est une fonction JavaScript qui est exécutée dès
+// qu'elle est définie.
+//
+// C'est un modèle de conception qui est également connu sous le nom de Fonction
+// anonyme auto-exécutable et contient deux parties principales. La première est
+// la fonction anonyme avec portée lexicale incluse dans l'opérateur de
+// groupement `()`. Cela empêche l'accès aux variables dans l'expression
+// idiomatique IIFE ainsi que la pollution de la portée globale.
+//
+// La deuxième partie crée la fonction immédiatement exécutable `()`, à travers
+// laquelle le moteur JavaScript interprétera directement la fonction.
+//
+//////////
+// SYNTAXE
+//
+// (function (…) { … }(…));
+// ou
+// (function (…) { … })(…);
+//
+////////////
+// EXEMPLE 1
+//
+// La fonction devient une expression de fonction qui est immédiatement
+// exécutée. La variable dans l'expression ne peut pas être atteinte de
+// l'extérieur.
+//
+//
+//
+////////////
+// EXEMPLE 2
+//
+// Affecter l'IIFE à une variable ne la stocke pas mais reçoit son résultat.
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+// RESSOURCES //////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+// - https://developer.mozilla.org/fr/docs/Glossary/IIFE
+//
